d1f62771d271ab4d27875c6911c07e778e630579
vadrille12 <109307947+vadrille12@users.noreply.github.com>
2022-12-23 21:17:55 +0200

    done task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const imageRef = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+    <img class="gallery__image"
+    src ='${preview}' 
+    alt ='${description}' 
+    data-source='${original}'
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", imageRef);
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`
+  );
+  instance.show();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") instance.close();
+  });
+}
 
 console.log(galleryItems);
